4a4aa6a4996135c372e9a3a236f657435c1dbff9
Nishant <i.nishantnikhil@gmail.com>
2020-01-31 01:04:34 +0530

    sim working

--- main.js
+++ main.js
@@ -3,6 +3,53 @@ const videoWidth = 600;
 const videoHeight = 450;
 
 const dataStore = [];
+const new_d = [];
+var push_inside = false;
+i = 9
+nose = 0
+l_shoulder = 5
+r_shoulder = 6
+l_elbow = 7
+r_elbow = 8
+l_wrist = 9
+r_wrist = 10
+var check_now = false;
+
+new_p = []
+new_p.push([[ 1.0000000174977224, -7.740127205835279e-9 ],
+    [ 1.3027767730020683, -0.9985045256593263 ],
+    [1.4934678042389056, -1.7932263809358968]])
+new_p.push([[ 1.0000000094873478, -1.7013754849415363e-9 ],
+    [ 1.2887349773416759, -1.0950544255504067 ],
+    [1.5702571081524033, -1.2241917853382493]])
+new_p.push([[ 1.0000000053111089, 3.4145015703757053e-9  ],
+    [ 1.7153436280087835, -1.0849623568437243  ],
+    [1.4733517698542036, 0.058408755721124805]])
+
+function mse(A,B){
+        var mse=0;
+        for(j = 0; j < A.length; j++){
+            mse += Math.pow((A[j][0] - B[j][0]), 2);
+            mse += Math.pow((A[j][1] - B[j][1]), 2);
+            
+        }
+        return mse;
+    }
+
+function get_dist(new_p, transformed_points) {
+    min_index = 0;
+    val_min = 10000;
+    for (i=0; i < new_p.length; i++){
+        val_now = mse(new_p[i], transformed_points);
+        console.log(i, val_now);
+        if (val_min > val_now) {
+            min_index = i;
+            val_min = val_now;
+        }
+    }
+    console.log("Minimum distance is", val_min, min_index)
+}
+
 
 let selectedPartToTrack = 0;
 const parts = [ "nose","leftEye", "rightEye", "leftEar", "rightEar", "leftShoulder", "rightShoulder", "leftElbow", "rightElbow","leftWrist", "rightWrist","leftHip", "rightHip", "leftKnee", "rightKnee", "leftAnkle", "rightAnkle"];
@@ -220,6 +267,27 @@ function detectPoseInRealTime(video, net) {
             dataStore.shift();
             dataStore.push(pose[0]);
         }
+        if (push_inside == true) {
+            new_d.push(pose[0]);
+            push_inside = false;
+        }
+
+        if (check_now == true) {
+            i = 9
+            inp = [[dataStore[i].keypoints[nose].position.x, dataStore[i].keypoints[nose].position.y],
+             [dataStore[i].keypoints[l_shoulder].position.x, dataStore[i].keypoints[r_shoulder].position.y],
+             [dataStore[i].keypoints[r_shoulder].position.x, dataStore[i].keypoints[r_shoulder].position.y]]
+
+            matrix = TransformationMatrix.fromTriangles(inp, [[0,1], [-1, 0], [1, 0]])
+            points_to_transform_right = 
+            [[dataStore[i].keypoints[r_shoulder].position.x, dataStore[i].keypoints[r_shoulder].position.y],
+             [dataStore[i].keypoints[r_elbow].position.x, dataStore[i].keypoints[r_elbow].position.y],
+             [dataStore[i].keypoints[r_wrist].position.x, dataStore[i].keypoints[r_wrist].position.y],
+             ]
+            transformed_points = TransformationMatrix.applyToPoints(matrix, points_to_transform_right)
+            get_dist(new_p, transformed_points)
+        }
+        
 
 
 
@@ -292,9 +360,14 @@ async function bindPage() {
 
 bindPage();
 
-// i=0
 // data_out = []
 // for (j=0; j < 17; j++) {
 //   data_out.push([dataStore[i].keypoints[j].position.x, dataStore[i].keypoints[j].position.y, dataStore[i].keypoints[j].score]);
 // }
 // for(i=0;i<data_out.length; i++){console.log("[ ", data_out[i][0],", ", data_out[i][1], ", ", data_out[i][2], " ],");}
+
+
+// [[dataStore[i].keypoints[r_shoulder].position.x, dataStore[i].keypoints[r_shoulder].position.y],
+//  [dataStore[i].keypoints[r_elbow].position.x, dataStore[i].keypoints[r_elbow].position.y],
+//  [dataStore[i].keypoints[r_wrist].position.x, dataStore[i].keypoints[r_wrist].position.y],
+//  ]
